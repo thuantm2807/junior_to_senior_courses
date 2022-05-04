01cab857d4a2981582c09179a942551effa5c699
thuan <thuantm2807@gmail.com>
2022-05-04 16:56:46 +0700

    Updates

---
+++ precache-manifest.d7c6fd9d9ea0ea9983a57b8432ab6738.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "539ac47fd6dddb4cc3aa",
+    "url": "/junior_to_senior_courses/static/css/main.7c3794df.chunk.css"
+  },
+  {
+    "revision": "539ac47fd6dddb4cc3aa",
+    "url": "/junior_to_senior_courses/static/js/main.3ec3ab63.chunk.js"
+  },
+  {
+    "revision": "7f6a8dd9c93b44e252ce",
+    "url": "/junior_to_senior_courses/static/js/runtime~main.0d67ef5f.js"
+  },
+  {
+    "revision": "3f58394dc47759aa1558",
+    "url": "/junior_to_senior_courses/static/css/2.122463c0.chunk.css"
+  },
+  {
+    "revision": "3f58394dc47759aa1558",
+    "url": "/junior_to_senior_courses/static/js/2.738473dc.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/junior_to_senior_courses/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "014a0a4e52e3774d7540d5d17b3c85c6",
+    "url": "/junior_to_senior_courses/static/media/NeNe_WeNo_Reliev.014a0a4e.woff"
+  },
+  {
+    "revision": "624a47e3d8b0fb840f60337418765150",
+    "url": "/junior_to_senior_courses/index.html"
+  }
+];
